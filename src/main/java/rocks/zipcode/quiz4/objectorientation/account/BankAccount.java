6c3315c8150e2733d90655d8f1bb9382b0dafc68
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-03-20 17:07:53 -0400

    last one to do is generics... my mortal foe

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,28 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Account implements Transactable {
+    Double balance = 0.;
     public void setBalance(Double val) {
+        this.balance = val;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        if(amountToIncreaseBy >= 0) {
+            balance += amountToIncreaseBy;
+        } else throw new IllegalArgumentException("must be over 0");
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        if(amountToDecreaseBy > 0 && amountToDecreaseBy <= this.balance) {
+            balance -= amountToDecreaseBy;
+        }else throw new IllegalArgumentException("must be over 0");
+    }
+
+    @Override
+    public Double getBalance() {
+        return balance;
     }
 }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -3,18 +3,62 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 30/12/2018.
  */
-public class Employee {
+public class Employee implements Worker,Transactable{
+    BankAccount account;
+    Double hoursWorked ;
+    Double hourlyWage;
     public Employee() {
+        this.account = new BankAccount();
+        this.hoursWorked=0.;
+        this.hourlyWage=35.;
     }
 
     public Employee(BankAccount bankAccount) {
+        this.account=bankAccount;
+        this.hourlyWage=35.;
+        this.hoursWorked=0.;
     }
 
     public BankAccount getBankAccount() {
-        return null;
+        return account;
     }
 
     public void setBankAccount(BankAccount bankAccount) {
+    this.account=bankAccount;
+    }
 
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        this.account.deposit(amountToIncreaseBy);
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        this.account.withdrawal(amountToDecreaseBy);
+    }
+
+    @Override
+    public Double getBalance() {
+        return this.account.balance;
+    }
+
+    @Override
+    public void increaseHoursWorked(Double numberOfHours) {
+        this.hoursWorked+=numberOfHours;
+    }
+
+    @Override
+    public Double getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    @Override
+    public Double getHourlyWage() {
+        return this.hourlyWage;
+    }
+
+    @Override
+    public Double getMoneyEarned() {
+        return hoursWorked*hourlyWage;
     }
 }
