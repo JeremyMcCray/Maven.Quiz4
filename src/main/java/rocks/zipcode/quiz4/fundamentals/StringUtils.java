08dd84c9eedd844f53dec6fae3b541d84552b858
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-03-20 15:48:26 -0400

    past the halfway mark

--- src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/StringUtils.java
@@ -10,7 +10,20 @@ public class StringUtils {
     }
 
     public static String capitalizeMiddleCharacter(String str) {
-        return null;
+        String ans = "";
+        if (str.length()>1){
+            for (int i = 0; i < str.length() / 2; i++) {
+                ans += str.charAt(i);
+            }
+            String mid = "" + str.charAt(str.length() / 2 + 1);
+            ans += mid.toUpperCase();
+            for (int i = str.length() / 2 + 1; i < str.length(); i++) {
+                ans += str.charAt(i);
+            }
+        } else {
+            ans+=str.toUpperCase();
+        }
+        return ans;
     }
 
     public static String lowerCaseMiddleCharacter(String str) {
@@ -22,11 +35,25 @@ public class StringUtils {
     }
 
     public static Boolean hasDuplicateConsecutiveCharacters(String str) {
-        return null;
+        char[] strToCharArr = str.toCharArray();
+        for (int i = 0; i < strToCharArr.length-1; i++) {
+            if(strToCharArr[i] == strToCharArr[i+1]){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String removeConsecutiveDuplicateCharacters(String str) {
-        return null;
+        String ans = "";
+        char[] strToCharArr = str.toCharArray();
+        for (int i = 0; i < strToCharArr.length-1; i++) {
+            if(!(strToCharArr[i] == strToCharArr[i+1] )){
+                ans+=strToCharArr[i];
+            }else{i++;}
+        }
+        ans+=str.charAt(str.length()-1);
+        return ans;
     }
 
     public static String invertCasing(String str) {
